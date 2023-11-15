1cb8617226a252d3e7e74a3816459236fdf1fee0
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-15 18:57:04 +0800

    update,

---
+++ hello_default-tryout_node20_scss/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang='scss'>
+/*每个页面公共css */
+
+.helloworld_scss {
+	a {
+		display: block;
+		padding: 6px 12px;
+		text-decoration: none;
+	}
+}
+</style>
