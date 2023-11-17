248aabe55b08a40389a0d329bcb9e80d26f5b3e2
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-17 18:30:07 +0800

    update,

--- emall/macrozheng/app-test-migrate/src/pages/public/register.vue
+++ emall/macrozheng/app-test-migrate/src/pages/public/register.vue
@@ -1,143 +1,145 @@
 <template>
-	<view class="container">
-		<view class="left-bottom-sign"></view>
-		<view class="back-btn yticon icon-zuojiantou-up" @click="navBack"></view>
-		<view class="right-top-sign"></view>
-		<!-- 设置白色背景防止软键盘把下部绝对定位元素顶上来盖住输入框等 -->
-		<view class="wrapper">
-			<view class="empty">
-				<image src="/static/qrcode_for_macrozheng_258.jpg" mode="aspectFit"></image>
-				<view class="empty-tips">
-					扫描上方二维码<view class="navigator">关注公众号</view>，
-				</view>
-				<view class="empty-tips">
-					回复<view class="navigator">会员</view>获取体验账号。
-				</view>
-			</view>
-		</view>
-	</view>
+  <view class="container">
+    <view class="left-bottom-sign"></view>
+    <view class="back-btn yticon icon-zuojiantou-up" @click="navBack"></view>
+    <view class="right-top-sign"></view>
+    <!-- 设置白色背景防止软键盘把下部绝对定位元素顶上来盖住输入框等 -->
+    <view class="wrapper">
+      <view class="empty">
+        <image
+          src="/static/qrcode_for_macrozheng_258.jpg"
+          mode="aspectFit"
+        ></image>
+        <view class="empty-tips">
+          扫描上方二维码<view class="navigator">关注公众号</view>，
+        </view>
+        <view class="empty-tips">
+          回复<view class="navigator">会员</view>获取体验账号。
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {}
-		},
-		onLoad() {
-		},
-		methods: {
-			navBack() {
-				uni.navigateBack();
-			},
-		},
-
-	}
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {
+    navBack() {
+      uni.navigateBack();
+    },
+  },
+};
 </script>
 
-<style lang='scss'>
-	page {
-		background: #fff;
-	}
-	
-	.empty {
-		position: fixed;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100vh;
-		padding-bottom: 100upx;
-		display: flex;
-		justify-content: center;
-		flex-direction: column;
-		align-items: center;
-		background: #fff;
-	
-		image {
-			width: 420upx;
-			height: 420upx;
-			margin-bottom: 30upx;
-		}
-		.empty-tips {
-			display: flex;
-			font-size: $font-sm+16upx;
-			color: $font-color-disabled;
-		
-			.navigator {
-				color: $uni-color-primary;
-				margin-left: 0upx;
-			}
-		}
-	}
+<style lang="scss">
+page {
+  background: #fff;
+}
 
-	.container {
-		padding-top: 115px;
-		position: relative;
-		width: 100vw;
-		height: 100vh;
-		overflow: hidden;
-		background: #fff;
-	}
+.empty {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  padding-bottom: 100upx;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background: #fff;
 
-	.wrapper {
-		position: relative;
-		z-index: 90;
-		background: #fff;
-		padding-bottom: 40upx;
-	}
+  image {
+    width: 420upx;
+    height: 420upx;
+    margin-bottom: 30upx;
+  }
+  .empty-tips {
+    display: flex;
+    font-size: $font-sm + 16upx;
+    color: $font-color-disabled;
 
-	.back-btn {
-		position: absolute;
-		left: 40upx;
-		z-index: 9999;
-		padding-top: var(--status-bar-height);
-		top: 40upx;
-		font-size: 40upx;
-		color: $font-color-dark;
-	}
+    .navigator {
+      color: $uni-color-primary;
+      margin-left: 0upx;
+    }
+  }
+}
 
-	.left-top-sign {
-		font-size: 120upx;
-		color: $page-color-base;
-		position: relative;
-		left: -16upx;
-	}
+.container {
+  padding-top: 115px;
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background: #fff;
+}
 
-	.right-top-sign {
-		position: absolute;
-		top: 80upx;
-		right: -30upx;
-		z-index: 95;
+.wrapper {
+  position: relative;
+  z-index: 90;
+  background: #fff;
+  padding-bottom: 40upx;
+}
 
-		&:before,
-		&:after {
-			display: block;
-			content: "";
-			width: 400upx;
-			height: 80upx;
-			background: #b4f3e2;
-		}
+.back-btn {
+  position: absolute;
+  left: 40upx;
+  z-index: 9999;
+  padding-top: var(--status-bar-height);
+  top: 40upx;
+  font-size: 40upx;
+  color: $font-color-dark;
+}
 
-		&:before {
-			transform: rotate(50deg);
-			border-radius: 0 50px 0 0;
-		}
+.left-top-sign {
+  font-size: 120upx;
+  color: $page-color-base;
+  position: relative;
+  left: -16upx;
+}
 
-		&:after {
-			position: absolute;
-			right: -198upx;
-			top: 0;
-			transform: rotate(-50deg);
-			border-radius: 50px 0 0 0;
-			/* background: pink; */
-		}
-	}
+.right-top-sign {
+  position: absolute;
+  top: 80upx;
+  right: -30upx;
+  z-index: 95;
 
-	.left-bottom-sign {
-		position: absolute;
-		left: -270upx;
-		bottom: -320upx;
-		border: 100upx solid #d0d1fd;
-		border-radius: 50%;
-		padding: 180upx;
-	}
+  &:before,
+  &:after {
+    display: block;
+    content: "";
+    width: 400upx;
+    height: 80upx;
+    background: #b4f3e2;
+  }
+
+  &:before {
+    transform: rotate(50deg);
+    border-radius: 0 50px 0 0;
+  }
+
+  &:after {
+    position: absolute;
+    right: -198upx;
+    top: 0;
+    transform: rotate(-50deg);
+    border-radius: 50px 0 0 0;
+    /* background: pink; */
+  }
+}
+
+.left-bottom-sign {
+  position: absolute;
+  left: -270upx;
+  bottom: -320upx;
+  border: 100upx solid #d0d1fd;
+  border-radius: 50%;
+  padding: 180upx;
+}
 </style>
+
