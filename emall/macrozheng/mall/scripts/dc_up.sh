ded016504e99203bcbe1ab892a96c326da0ca3fc
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-19 13:05:46 +0800

    update,

--- emall/macrozheng/mall/scripts/dc_up.sh
+++ emall/macrozheng/mall/scripts/dc_up.sh
@@ -47,21 +47,36 @@ docker compose -f docker-compose-env.yml up -d --force-recreate --remove-orphans
  
 # sleep 1
 
-# docker cp document/sql/mall.sql mysql:/
+# docker cp document/sql/tbl*.sql mysql:/
+# docker cp ./document/sql/tbl_cms.sql mysql:/
+# docker cp ./document/sql/tbl_oms.sql mysql:/
+# docker cp ./document/sql/tbl_pms.sql mysql:/
+# docker cp ./document/sql/tbl_sms.sql mysql:/
+# docker cp ./document/sql/tbl_ums.sql mysql:/
 
 # docker compose exec -it mysql /bin/bash
 # docker compose logs -f mysql
 
 # #进入mysql容器
 # docker compose exec -it mysql /bin/bash
+
 # #连接到mysql服务
 # mysql -uroot -proot --default-character-set=utf8
+
 # #创建远程访问用户
 # CREATE USER 'reader'@'%' IDENTIFIED WITH authentication_plugin BY '123456';
+
 # grant all privileges on *.* to 'reader'@'%';
 # #创建mall数据库
 # create database mall character set utf8;
+
 # #使用mall数据库
 # use mall;
+
 # #导入mall.sql脚本
-# source /mall.sql;
+# source /mall.sql; (abonded)
+# source /tbl_cms.sql;
+# source /tbl_oms.sql;
+# source /tbl_pms.sql;
+# source /tbl_sms.sql;
+# source /tbl_ums.sql;
