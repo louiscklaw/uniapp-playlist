c5cba6d977e798594639d11b49b7815e97719507
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-19 13:05:21 +0800

    update,

---
+++ emall/macrozheng/mall/document/sql/tbl_cms.sql
@@ -0,0 +1,284 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50719
+ Source Host           : localhost:3306
+ Source Schema         : mall
+
+ Target Server Type    : MySQL
+ Target Server Version : 50719
+ File Encoding         : 65001
+
+ Date: 11/05/2023 15:48:15
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cms_help
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_help`;
+CREATE TABLE `cms_help`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NULL DEFAULT NULL,
+  `icon` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `show_status` int(1) NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `read_count` int(1) NULL DEFAULT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '帮助表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_help
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cms_help_category
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_help_category`;
+CREATE TABLE `cms_help_category`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `icon` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分类图标',
+  `help_count` int(11) NULL DEFAULT NULL COMMENT '专题数量',
+  `show_status` int(2) NULL DEFAULT NULL,
+  `sort` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '帮助分类表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_help_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cms_member_report
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_member_report`;
+CREATE TABLE `cms_member_report`  (
+  `id` bigint(20) NULL DEFAULT NULL,
+  `report_type` int(1) NULL DEFAULT NULL COMMENT '举报类型：0->商品评价；1->话题内容；2->用户评论',
+  `report_member_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '举报人',
+  `create_time` datetime NULL DEFAULT NULL,
+  `report_object` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `report_status` int(1) NULL DEFAULT NULL COMMENT '举报状态：0->未处理；1->已处理',
+  `handle_status` int(1) NULL DEFAULT NULL COMMENT '处理结果：0->无效；1->有效；2->恶意',
+  `note` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户举报表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_member_report
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cms_prefrence_area
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_prefrence_area`;
+CREATE TABLE `cms_prefrence_area`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sub_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `pic` varbinary(500) NULL DEFAULT NULL COMMENT '展示图片',
+  `sort` int(11) NULL DEFAULT NULL,
+  `show_status` int(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '优选专区' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_prefrence_area
+-- ----------------------------
+INSERT INTO `cms_prefrence_area` VALUES (1, '让音质更出众', '音质不打折 完美现场感', NULL, NULL, 1);
+INSERT INTO `cms_prefrence_area` VALUES (2, '让音质更出众22', '让音质更出众22', NULL, NULL, NULL);
+INSERT INTO `cms_prefrence_area` VALUES (3, '让音质更出众33', NULL, NULL, NULL, NULL);
+INSERT INTO `cms_prefrence_area` VALUES (4, '让音质更出众44', NULL, NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for cms_prefrence_area_product_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_prefrence_area_product_relation`;
+CREATE TABLE `cms_prefrence_area_product_relation`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `prefrence_area_id` bigint(20) NULL DEFAULT NULL,
+  `product_id` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '优选专区和产品关系表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_prefrence_area_product_relation
+-- ----------------------------
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (1, 1, 12);
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (2, 1, 13);
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (3, 1, 14);
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (4, 1, 18);
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (5, 1, 7);
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (6, 2, 7);
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (7, 1, 22);
+INSERT INTO `cms_prefrence_area_product_relation` VALUES (24, 1, 23);
+
+-- ----------------------------
+-- Table structure for cms_subject
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_subject`;
+CREATE TABLE `cms_subject`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NULL DEFAULT NULL,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `pic` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '专题主图',
+  `product_count` int(11) NULL DEFAULT NULL COMMENT '关联产品数量',
+  `recommend_status` int(1) NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `collect_count` int(11) NULL DEFAULT NULL,
+  `read_count` int(11) NULL DEFAULT NULL,
+  `comment_count` int(11) NULL DEFAULT NULL,
+  `album_pics` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '画册图片用逗号分割',
+  `description` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `show_status` int(1) NULL DEFAULT NULL COMMENT '显示状态：0->不显示；1->显示',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `forward_count` int(11) NULL DEFAULT NULL COMMENT '转发数',
+  `category_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '专题分类名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '专题表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_subject
+-- ----------------------------
+INSERT INTO `cms_subject` VALUES (1, 1, 'polo衬衫的也时尚', NULL, NULL, NULL, '2018-11-11 13:26:55', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '服装专题');
+INSERT INTO `cms_subject` VALUES (2, 2, '大牌手机低价秒', NULL, NULL, NULL, '2018-11-12 13:27:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '手机专题');
+INSERT INTO `cms_subject` VALUES (3, 2, '晓龙845新品上市', NULL, NULL, NULL, '2018-11-13 13:27:05', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '手机专题');
+INSERT INTO `cms_subject` VALUES (4, 1, '夏天应该穿什么', NULL, NULL, NULL, '2018-11-01 13:27:09', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '服装专题');
+INSERT INTO `cms_subject` VALUES (5, 1, '夏季精选', NULL, NULL, NULL, '2018-11-06 13:27:18', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '服装专题');
+INSERT INTO `cms_subject` VALUES (6, 2, '品牌手机降价', NULL, NULL, NULL, '2018-11-07 13:27:21', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '手机专题');
+
+-- ----------------------------
+-- Table structure for cms_subject_category
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_subject_category`;
+CREATE TABLE `cms_subject_category`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `icon` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分类图标',
+  `subject_count` int(11) NULL DEFAULT NULL COMMENT '专题数量',
+  `show_status` int(2) NULL DEFAULT NULL,
+  `sort` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '专题分类表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_subject_category
+-- ----------------------------
+INSERT INTO `cms_subject_category` VALUES (1, '服装专题', NULL, NULL, NULL, NULL);
+INSERT INTO `cms_subject_category` VALUES (2, '手机专题', NULL, NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for cms_subject_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_subject_comment`;
+CREATE TABLE `cms_subject_comment`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `subject_id` bigint(20) NULL DEFAULT NULL,
+  `member_nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `member_icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `content` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `show_status` int(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '专题评论表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_subject_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cms_subject_product_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_subject_product_relation`;
+CREATE TABLE `cms_subject_product_relation`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `subject_id` bigint(20) NULL DEFAULT NULL,
+  `product_id` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 71 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '专题商品关系表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_subject_product_relation
+-- ----------------------------
+INSERT INTO `cms_subject_product_relation` VALUES (1, 1, 12);
+INSERT INTO `cms_subject_product_relation` VALUES (2, 1, 13);
+INSERT INTO `cms_subject_product_relation` VALUES (3, 1, 14);
+INSERT INTO `cms_subject_product_relation` VALUES (4, 1, 18);
+INSERT INTO `cms_subject_product_relation` VALUES (5, 1, 7);
+INSERT INTO `cms_subject_product_relation` VALUES (6, 2, 7);
+INSERT INTO `cms_subject_product_relation` VALUES (7, 1, 22);
+INSERT INTO `cms_subject_product_relation` VALUES (29, 1, 23);
+INSERT INTO `cms_subject_product_relation` VALUES (30, 4, 23);
+INSERT INTO `cms_subject_product_relation` VALUES (31, 5, 23);
+INSERT INTO `cms_subject_product_relation` VALUES (68, 2, 26);
+INSERT INTO `cms_subject_product_relation` VALUES (69, 3, 26);
+INSERT INTO `cms_subject_product_relation` VALUES (70, 6, 26);
+
+-- ----------------------------
+-- Table structure for cms_topic
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_topic`;
+CREATE TABLE `cms_topic`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `start_time` datetime NULL DEFAULT NULL,
+  `end_time` datetime NULL DEFAULT NULL,
+  `attend_count` int(11) NULL DEFAULT NULL COMMENT '参与人数',
+  `attention_count` int(11) NULL DEFAULT NULL COMMENT '关注人数',
+  `read_count` int(11) NULL DEFAULT NULL,
+  `award_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '奖品名称',
+  `attend_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '参与方式',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '话题内容',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '话题表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_topic
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cms_topic_category
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_topic_category`;
+CREATE TABLE `cms_topic_category`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `icon` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分类图标',
+  `subject_count` int(11) NULL DEFAULT NULL COMMENT '专题数量',
+  `show_status` int(2) NULL DEFAULT NULL,
+  `sort` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '话题分类表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_topic_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cms_topic_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_topic_comment`;
+CREATE TABLE `cms_topic_comment`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `member_nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `topic_id` bigint(20) NULL DEFAULT NULL,
+  `member_icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `content` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `show_status` int(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '专题评论表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cms_topic_comment
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
