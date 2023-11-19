5776cc9d57cf1e792d9e72faee75bb4e2e105e49
Louis Law <louiscklaw@users.noreply.github.com>
2023-11-19 11:54:20 +0800

    update,

--- emall/macrozheng/dc_up.sh
+++ emall/macrozheng/dc_up.sh
@@ -10,10 +10,14 @@ pushd mall
 popd
 
 docker compose pull
+docker compose build
+
+docker compose kill
+docker compose down
 
 docker compose up -d --force-recreate
 
 # docker compose logs -f mall-app-web
 
 cd mall
-  docker compose logs -f
+  docker compose logs -f mall-search elasticsearch
